24b5584bb3d4f0da7459f94f0b4ee67475c34ee8
Andy Ramaroson <andyramaroson@gmail.com>
2023-02-17 21:55:04 +0100

    [setup] add CNAME

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,8 +1,20 @@
+import Card from '../../components/Card/Card'
 function Home() {
   return (
     <div>
       <h1>AndyRB</h1>
-      <p>Home page test</p>
+      <Card
+        image="https://example.com/image.jpg"
+        title="Beautiful Beach House"
+        description="Relax and enjoy the sound of the waves in this stunning beachfront home."
+        price={200}
+      />
+      <Card
+        image="https://example.com/image.jpg"
+        title="Beautiful Beach House"
+        description="Relax and enjoy the sound of the waves in this stunning beachfront home."
+        price={200}
+      />
     </div>
   )
 }
