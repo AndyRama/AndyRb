13d1689f24ebf36a0191e71ddc7104376351d027
Andy Ramaroson <andyramaroson@gmail.com>
2023-05-04 23:56:13 +0200

    [enhancement] add home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,69 @@
 import './Home.scss'
+import HomeHeaderCard from './HomeHeaderCard'
+import HomeHosting from './HomeHosting'
+import HomeExperiences from './HomeExperiences'
+import HomeGift from './HomeGift'
 
-function Home() {
+const Home = () => {
   return (
-    <div className="Home">
-      <h1>Home page</h1>
-    </div>
+    <>
+      <header className="h__header">
+        <TmpHeaderMenu />
+        <HomeHeaderCard />
+      </header>
+      <main className="h__main">
+        <HomeExperiences />
+        <HomeGift />
+        <HomeHosting />
+        <TmpInspiration />
+      </main>
+      <footer className="h__footer">
+        <TmpFooter />
+      </footer>
+    </>
   )
 }
 
 export default Home
+
+// Composants temporaires, à remplacer par les vrais
+const TmpHeaderMenu = () => (
+  <div
+    style={{
+      position: 'fixed',
+      width: '70vw',
+      minHeight: '10vmin',
+      backgroundColor: 'white',
+      color: 'black',
+      borderRadius: '50px',
+      margin: '3vmin calc((100% - 70vw)/4)',
+      display: 'grid',
+      alignItems: 'center',
+      justifyItems: 'center',
+    }}
+  >
+    header menu
+  </div>
+)
+const TmpFooter = () => (
+  <div
+    style={{
+      width: '100%',
+      minHeight: '5rem',
+      textAlign: 'center',
+    }}
+  >
+    footer
+  </div>
+)
+const TmpInspiration = () => (
+  <div
+    style={{
+      width: '100%',
+      minHeight: '5rem',
+      textAlign: 'center',
+    }}
+  >
+    inspiration
+  </div>
+)
