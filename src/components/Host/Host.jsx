a9096228747a7fc822c59db77f1742606c919441
Andy Ramaroson <andyramaroson@gmail.com>
2023-05-17 23:33:10 +0200

    (feat)refactor and cleaning proposal step 1

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -8,59 +8,22 @@ const Host = () => {
   return (
     <>
       <header className="h__header">
-        {/* <TmpHeaderMenu /> */}
         <HostHeaderCard />
       </header>
       <main className="h__main">
         <HostExperiences />
         <HostGift />
         <HostHosting />
-        {/* <TmpInspiration /> */}
+        <div
+          style={{
+            width: '100%',
+            minHeight: '5rem',
+            textAlign: 'center',
+          }}
+        ></div>
       </main>
     </>
   )
 }
 
 export default Host
-
-// Composants temporaires, à remplacer par les vrais
-const TmpHeaderMenu = () => (
-  <div
-    style={{
-      position: 'fixed',
-      width: '70vw',
-      minHeight: '10vmin',
-      backgroundColor: 'white',
-      color: 'black',
-      borderRadius: '50px',
-      margin: '3vmin calc((100% - 70vw)/4)',
-      display: 'grid',
-      alignItems: 'center',
-      justifyItems: 'center',
-    }}
-  >
-    header menu
-  </div>
-)
-const TmpFooter = () => (
-  <div
-    style={{
-      width: '100%',
-      minHeight: '5rem',
-      textAlign: 'center',
-    }}
-  >
-    footer
-  </div>
-)
-const TmpInspiration = () => (
-  <div
-    style={{
-      width: '100%',
-      minHeight: '5rem',
-      textAlign: 'center',
-    }}
-  >
-    inspiration
-  </div>
-)
